301a9c220d39ff26f24fdb3b8e83188d7c87cc9c
Marc Bössow <marc@boessow.one>
2021-04-19 08:25:12 +0200

    add database functions real

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+    define('DB_TYPE', 'mysql');
+    define('DB_HOST', 'lx6.hoststar.hosting');
+    define('DB_NAME', 'ch217930_m120');
+    define('DB_USER', 'ch217930_m120');
+    define('DB_PASS', 'M120!Test');
+
+?>
